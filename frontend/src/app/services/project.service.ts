569e6d4a3374b5ec53056d24b15a4e9cc2889360
Nemanja Lukic <lukic.sr51.2022@uns.ac.rs>
2024-11-09 21:07:52 +0100

    fixed project maping on front end

--- frontend/src/app/services/project.service.ts
+++ frontend/src/app/services/project.service.ts
@@ -1,13 +1,13 @@
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import {map, Observable} from "rxjs";
 import { Project } from "../model/project";
 
 @Injectable({
     providedIn: 'root'
   })
 export class ProjectService{
-    private apiUrl = "http://localhost:8001/api" 
+    private apiUrl = "http://localhost:8001/api"
     constructor(private http:HttpClient){}
 
     createProject(project: Project): Observable<Project> {
@@ -15,10 +15,17 @@ export class ProjectService{
           headers: new HttpHeaders({ 'Content-Type': 'application/json' })
         });
       }
-    
-      getAllProjects(): Observable<Project[]> {
-        return this.http.get<Project[]>(`${this.apiUrl}/projects`);
-    }
+
+  getAllProjects(): Observable<Project[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/projects`).pipe(
+      map((data: any[]) => data.map(item => new Project(
+        item.name,
+        new Date(item.completionDate),
+        item.minMembers,
+        item.maxMembers
+      )))
+    );
+  }
 
     deleteProjectById(id:number): Observable<void>{
       return this.http.delete<void>(`${this.apiUrl}/projects/${id}`)
