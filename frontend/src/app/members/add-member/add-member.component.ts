f04d420af1a9ebbe76b627c2b41e780c891e4c83
neradinistaoduser-a <lukabogarkarg@gmail.com>
2024-11-27 18:22:52 +0100

    add-members_hotfix

--- frontend/src/app/members/add-member/add-member.component.ts
+++ frontend/src/app/members/add-member/add-member.component.ts
@@ -5,6 +5,8 @@ import { ProjectService } from '../../services/project.service';
 import { UserFP } from '../../model/userForProject';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Project } from '../../model/project';
+import { TaskService } from '../../services/task.service';
+import { Task } from '../../model/task';
 
 @Component({
   selector: 'app-add-member',
@@ -17,6 +19,8 @@ export class AddMemberComponent implements OnInit {
   project: Project | null = null;
   maximumNumber: boolean = false;
   userAlreadyExists: boolean = false; // Dodato
+  tasks : Task[] | null = null; 
+  cannotAddmemberToProject :boolean = false;
 
   constructor(
     private fb: FormBuilder,
@@ -24,6 +28,7 @@ export class AddMemberComponent implements OnInit {
     private projectService: ProjectService,
     private route: ActivatedRoute,
     private router:Router,
+    private taskService:TaskService,
   ) {
     this.addMemberForm = this.fb.group({
       username: ['', [Validators.required, Validators.minLength(3)]]
@@ -36,33 +41,38 @@ export class AddMemberComponent implements OnInit {
     this.id = this.route.snapshot.paramMap.get('projectId');
     if (this.addMemberForm.valid) {
       this.userService.getUserByUsername(this.addMemberForm.value.username).subscribe((resp) => {
-        console.log('user resp: ' + JSON.stringify(resp), resp);
         let user: UserFP = new UserFP(resp.user.id, resp.user.username, resp.user.role);
-        console.log(user);
 
         if (this.id && user) {
           this.projectService.getById(this.id).subscribe((resp) => {
             this.project = resp;
+            
             this.userAlreadyExists = false; // Resetuje se pri svakom pokušaju dodavanja
-
-            if (this.project) {
-              console.log("Maxinum number bool1: ",this.maximumNumber)
-              this.maximumNumber = this.project.members.length >= this.project.maxMembers;
-              console.log("Maxinum number bool2: ",this.maximumNumber)
-              console.log("project members lenght: ",this.project.members.length)
-              console.log("project maxmembers: ",this.project.maxMembers)
-              // Provera da li korisnik već postoji u projektu
-              const exists = this.project.members.some(
-                (member) => member.id === user.id || member.username === user.username
-              );
-              if (exists) {
-                this.userAlreadyExists = true;
-              } else if (!this.maximumNumber && this.id) {
-                this.projectService
-                  .createMember(this.id, user)
-                  .subscribe(() => console.log('Member successfully added'));
-                  this.router.navigate(['/all-projects', this.id]);
-              }
+            if(this.id){
+              this.taskService.getAllTasksByProjectId(this.id).subscribe((resp)=>{
+                this.tasks = Array.isArray(resp.tasks) ? resp.tasks : Array.from(resp.tasks);
+                console.log(this.tasks);
+                if (this.project) {
+                  this.maximumNumber = this.project.members.length >= this.project.maxMembers;
+                  // Provera da li korisnik već postoji u projektu
+                  const exists = this.project.members.some(
+                    (member) => member.id === user.id || member.username === user.username
+                  );
+                  
+                  if(this.tasks){
+                    this.cannotAddmemberToProject = this.tasks.every(task => task.status === 'Done');
+                    console.log(this.cannotAddmemberToProject)
+                  }
+                  if (exists) {
+                    this.userAlreadyExists = true;
+                  } else if (!this.maximumNumber && this.id && !this.cannotAddmemberToProject) {
+                    this.projectService
+                      .createMember(this.id, user)
+                      .subscribe(() => console.log('Member successfully added'));
+                      this.router.navigate(['/all-projects', this.id]);
+                  }
+                }
+              })
             }
           });
         }
